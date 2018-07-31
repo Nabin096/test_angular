8e27ca22928cd345f0f15c8510db1c97a0bb7016
Nabin <nabin.kundu96@gmail.com>
2018-07-31 16:30:09 +0530

    angular push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { FormsModule } from '@angular/forms';
+import { ElementDetailComponent } from './element-detail/element-detail.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogsComponent,
+    ElementDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
